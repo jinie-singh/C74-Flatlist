5258001a464fe079666604bcfc4b4918035d8412
jinie-singh <anjani.1997mpvm@gmail.com>
2021-09-03 13:57:26 +0530

    Added

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View,ScrollView } from 'react-native';
+import { Text, View,ScrollView,FlatList } from 'react-native';
 import * as firebase from "firebase";
 import db from '../config'
 
@@ -7,7 +7,8 @@ export default class Searchscreen extends React.Component {
     constructor(props){
       super(props);
       this.state = {
-        allTransactions : []
+        allTransactions : [],
+        lastVisibleTransaction : null
       }
     }
     componentDidMount = async () => {
@@ -18,22 +19,31 @@ export default class Searchscreen extends React.Component {
         })
       })
     }
+    fetchMoreTransactions = async()=>{
+      const query = await db.collection("transactions").startAfter(this.state.lastVisibleTransaction).limit(10).get();
+      query.docs.map((doc)=>{
+        this.setState({
+          allTransactions : [...this.state.allTransactions,doc.data()],
+          lastVisibleTransaction : doc
+        })
+      })
+    }
     render() {
       return (
-        <ScrollView>
-          {
-            this.state.allTransactions.map((transaction)=>{
-              return(
-                <View style = {{borderBottomWidth : 2}}>
-                  <Text>{"Book Id : " + transaction.bookId}</Text>
-                  <Text>{"Student Id : " + transaction.studentId}</Text>
-                  <Text>{"Transaction Type : " + transaction.transactionType}</Text>
-                  <Text>{"Date : " + transaction.date.toDate()}</Text>
-                </View>
-              )
-            })
-          }
-        </ScrollView>
+        <FlatList 
+          data={this.state.allTransactions}
+          renderItem = {({item}) => (
+            <View style = {{borderBottomWidth : 2}}>
+                  <Text>{"Book Id : " + item.bookId}</Text>
+                  <Text>{"Student Id : " + item.studentId}</Text>
+                  <Text>{"Transaction Type : " + item.transactionType}</Text>
+                  <Text>{"Date : " + item.date.toDate()}</Text>
+            </View>
+          )}
+          keyExtractor = {(item,index) => index.toString()}
+          onEndReached = {this.fetchMoreTransactions}
+          onEndReachedThreshold = {0.7}
+        />
       );
     }
   }
